26ae3be86da22aab91a6e6d609e33ba52e19651d
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-08-08 12:37:15 +0100

    feat: button component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { Button } from "./Button";
+import { Button } from "./../components/core/Button/index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/Button",
+  title: "UI/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -14,7 +14,10 @@ const meta = {
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    block: { control: "boolean" },
+    loading: { control: "boolean" },
+    theme: { control: "radio" },
+    variant: { control: "radio" }
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -24,29 +27,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const PrimaryFilled: Story = {
+  render: (args) => <div className="w-[50dvw] flex justify-center"><Button {...args}>Primary 🤯</Button></div>,
   args: {
-    primary: true,
-    label: "Button",
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: "Button",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button",
+    theme: "primary",
+    variant: "filled",
+    size: "40"
   },
 };
