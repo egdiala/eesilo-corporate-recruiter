99cdec1fc8039df895607f12bb60588f64572bff
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2025-03-29 10:57:10 +0100

    feat: toast time

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Toaster position="top-center" toastOptions={{ unstyled: true, classNames: { toast: "right-0 left-0" }, duration: 8000 }} pauseWhenPageIsHidden />
+    <Toaster position="top-center" toastOptions={{ unstyled: true, classNames: { toast: "right-0 left-0" }, duration: 3000 }} pauseWhenPageIsHidden />
     <HelmetProvider>
       <App />
     </HelmetProvider>
